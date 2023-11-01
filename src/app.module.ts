fae1889b66ccefb31caddc2e04fcff54ab1b2805
yurchykyur <yurchyk.yur.s@gmail.com>
2023-11-01 21:39:19 +0200

    connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const environment: string = process.env.NODE_ENV || 'development';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const PORT = process.env.PORT || 3001;
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
+    await app.listen(PORT, () =>
+      console.log(`Server running. Use our API on port: ${PORT}`),
+    );
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 }
 bootstrap();
